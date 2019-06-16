cc86f7607591e2b886be6aa3e9e8e8dd2e016c97
Rajesh Mule <rajeshsmule@gmail.com>
2019-06-16 06:06:01 +0530

    complate this.js assignment.

--- assignments/this.js
+++ assignments/this.js
@@ -30,3 +30,97 @@ return obj;
 
 }
 
+//Using Object.create
+
+function createUser(name, id, password, noOfProject){
+    this.name = name;
+    this.id = id;
+    this.password = password;
+    this.noOfProject = noOfProject;
+}
+
+createUser.prototype.getUsername = function getUsername(){
+    return this.name;
+};
+
+createUser.prototype.getPassword = function getPassword(){
+    return this.password;
+};
+
+createUser.prototype.changeUsername = function changeUsername(){
+    return this.name;
+};
+
+createUser.prototype.incrementProject = function incrementProject(){
+    return this.noOfProject = ++(this.noOfProject);
+};
+
+createUser.prototype.decrementProject = function decrementProject(){
+    return this.noOfProject = --(this.noOfProject);
+};
+
+
+
+
+//Using Pseudoclassical Way
+
+
+function createUser(name, id, password,noOfProject){
+    
+    this.name = name;
+    this.id = id;
+    this.password = password;
+    this.noOfProject = noOfProject;
+}
+
+createUser.prototype.getUsername = function(){
+    return this.name;
+};
+
+createUser.prototype.getPassword = function(){
+    return this.password;
+};
+
+createUser.prototype.changeUsername = function(){
+    return this.name;
+};
+
+createUser.prototype.incrementProject = function(){
+    return this.noOfProject = ++(this.noOfProject);
+};
+
+createUser.prototype.decrementProject = function(){
+    return this.noOfProject = --(this.noOfProject);
+};
+
+
+
+//Using class
+
+class User {
+    constructor(name, id, password,noOfProject) {
+        this.name = name;
+        this.id = id;
+        this.password = password;
+        this.noOfProject = noOfProject;
+    }
+    getUsername() {
+        return this.name;
+    }
+    getPassword(){
+        return this.password;
+    };
+
+    changeUsername(){
+        return this.name;
+    };
+
+    incrementProject(){
+        return this.noOfProject = ++(this.noOfProject);
+    };
+
+    decrementProject(){
+        return this.noOfProject = --(this.noOfProject);
+    };
+    
+  }
